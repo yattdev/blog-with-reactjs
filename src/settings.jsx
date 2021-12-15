ccf6d5fa75bd5c93b936bd57ce7103f9a05c5cd6
yattdev <yattdeveloper@gmail.com>
2021-12-15 22:44:29 +0100

    UPdate settings
    - Added variable ROOTPATH, that conent project root path
    - Update backend_base_url to add '../api/v1'

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,3 +1,4 @@
-const backend_base_url = "http://localhost:8000"
+const backend_base_url = "http://localhost:8000/api/v1/";
+export const ROOTPATH = "./";
 
-export default backend_base_url
+export default backend_base_url;
