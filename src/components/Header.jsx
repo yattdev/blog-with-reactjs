974beee0cf06710608924221da35067d0761e8f5
yattdev <yattdeveloper@gmail.com>
2021-12-15 23:17:12 +0100

    Update Components/Header
    - To added some props, that's make dynamic with (image, title,
            sub_title), which change based on props provide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
 // import React from "react";
 
-const Header = () => {
+const Header = ({ image, title, sub_title }) => {
   return (
     <>
       {/*<!-- Page Header-->*/}
-      <header
-        class="masthead"
-        style={{ backgroundImage: "url('assets/img/home-bg.jpg')" }}
-      >
-        <div class="container position-relative px-4 px-lg-5">
-          <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-10 col-lg-8 col-xl-8">
-              <div class="site-heading">
-                <h1>Articles récents</h1>
-                <span class="subheading">Developpeurs et Entrepreneurs</span>
+      <header className="masthead" style={{ backgroundImage: `url(${image})` }}>
+        <div className="container position-relative px-4 px-lg-5">
+          <div className="row gx-4 gx-lg-5 justify-content-center">
+            <div className="col-md-10 col-lg-8 col-xl-8">
+              <div className="site-heading">
+                <h1>{title}</h1>
+                <h2 class="subheading">{sub_title}</h2>
               </div>
             </div>
           </div>
