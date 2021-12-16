786fc2a858354dfab0632a49068a17a924302cf4
yattdev <yattdeveloper@gmail.com>
2021-12-16 11:44:13 +0100

    Update article details page
    - To added: loading, and some useful variables

--- src/pages/blog/ArticleDetails.jsx
+++ src/pages/blog/ArticleDetails.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+// import React from "react";
+
+import { useParams } from "react-router-dom";
+import Header from "../../components/Header";
+import useFetch from "../../useFetch";
+import Parse from "html-react-parser";
+import { useGlobalContext } from "../../context";
 
 const ArticleDetails = () => {
-  return <>ArticleDetails</>;
+  const { articleUID } = useParams();
+  const { items } = useFetch("articles/" + articleUID);
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return <center>Loading...</center>;
+  }
+
+  if (!items) {
+    return (
+      <>
+        <center>Cette article n'exist pas.</center>
+      </>
+    );
+  }
+
+  const { image, title, sub_title, body } = items;
+  return (
+    <>
+      {/* Post Header */}
+      <Header image={image} title={title} sub_title={sub_title} />
+      {/*<!-- Post Content-->*/}
+      <article className="mb-4">
+        <div className="container px-5 px-lg-5">
+          <div className="row gx-4 gx-lg-2 justify-content-center">
+            <div className="col-md-10 col-lg-8 col-xl-7">{Parse(body)}</div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default ArticleDetails;
