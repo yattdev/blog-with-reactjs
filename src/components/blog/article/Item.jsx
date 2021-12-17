3e3db77233bde9ddb4094e33d7f638f334fd8281
yattdev <yattdeveloper@gmail.com>
2021-12-17 09:32:52 +0100

    Create directory blog/articles inside components dir to store blog article components
    - Created Item components to display single item

---
+++ src/components/blog/article/Item.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+import Moment from "moment";
+
+import { Link } from "react-router-dom";
+
+const Item = ({ id, title, category, author, date_published }) => {
+  Moment.locale("fr");
+  return (
+    <>
+      <div className="post-preview">
+        <Link to={`${"/articles/" + id}`}>
+          <h2 className="post-title">{title}</h2>
+          <h3 className="post-subtitle">{category}</h3>
+        </Link>
+        <p className="post-meta">
+          Par &nbsp;
+          <a href="#!">{author}</a> &nbsp;
+          {Moment(date_published).format("d MMM YYYY")}
+        </p>
+      </div>
+      {/* <!-- Divider-->*/}
+      <hr className="my-4" />
+    </>
+  );
+};
+
+export default Item;
