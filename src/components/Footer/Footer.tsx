daecf2a32b91e9d3bc2e2ee27f60955786bb00a1
yattdev <yattdeveloper@gmail.com>
2021-12-17 11:28:08 +0100

    Update Footer.tsx from components/Footer
    - TO change logo image to blog name

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,16 +13,10 @@ const Footer = () => {
           <div className="row">
             <div className="footer-col col-12 col-md-6 col-lg-5 mb-3">
               <h4>
-                <img
-                  className="logo-icon mb-2"
-                  src="assets/images/site-logo.svg"
-                  alt="logo"
-                />
+                    Starter
               </h4>
               <div>
-                About your company. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Pellentesque sit amet molestie dui. Praesent
-                viverra rhoncus metus, eget malesuada nunc.{" "}
+                Un blog dans le monde du tech, fondamentalement pour les developpeurs and entrepreneurs.
               </div>
               {/* SocialIcons */}
               <SocialIcons />
