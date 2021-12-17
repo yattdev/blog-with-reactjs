6820e2648e8e4fab62462576244a3ec5cc90d8e0
yattdev <yattdeveloper@gmail.com>
2021-12-17 09:34:27 +0100

    Create ItemList.jsx to components/blog/articles/ to display articles list

---
+++ src/components/blog/article/ItemList.jsx
@@ -0,0 +1,66 @@
+// import React from "react";
+
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "../../../context";
+import useFetch from "../../../useFetch";
+import Item from "./Item";
+
+const Contents = () => {
+  const [articles, setArticles] = useState([]);
+  const { items } = useFetch("articles");
+  const { loading } = useGlobalContext();
+  // console.log(items);
+
+  useEffect(() => {
+    if (items) {
+      setArticles(() => {
+        return items;
+      });
+    }
+  }, [items, articles]);
+
+  if (loading) {
+    return (
+      <>
+        <center>Loading...</center>
+      </>
+    );
+  }
+
+  if (articles.length === 0) {
+    return (
+      <section className="container px-4 px-lg-5">
+        {/* cart header */}
+        <header>
+          <h4 className="row gx-4 gx-lg-5 justify-content-center">
+            Sorry! No post yet
+          </h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      {/*<!-- Main Content-->*/}
+      <div className="container px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+          <div className="col-md-10 col-lg-8 col-xl-7">
+            {/* <!-- Post preview-->*/}
+            {articles.map((item) => {
+              return <Item key={item.id} {...item} />;
+            })}
+            {/* <!-- Pager-->*/}
+            <div className="d-flex justify-content-end mb-4">
+              <a className="btn btn-primary text-uppercase" href="#!">
+                Tout les posts →
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contents;
