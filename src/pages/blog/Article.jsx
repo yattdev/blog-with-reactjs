3c422ba4f39cb8a9c080423216c1d94c6354db35
yattdev <yattdeveloper@gmail.com>
2021-12-17 14:27:45 +0100

    Update Article pages
    - To define default value for title, image and sub_title

--- src/pages/blog/Article.jsx
+++ src/pages/blog/Article.jsx
@@ -1,14 +1,19 @@
 // import React from "react";
 import Header from "../../components/Header";
 import PaginatedItems from "../../components/utils/PaginatedItems";
+import HeaderImage from "../../assets/img/website_progess.png";
 
 const Article = () => {
   return (
     <>
       {/*<!-- Header -->*/}
-      <Header />
+      <Header
+        title="Blog en developpement..."
+        sub_title="Technologie blog, for developers and Entrepreneurs"
+        image={HeaderImage}
+      />
       {/*<!-- ItemList -->*/}
-      <PaginatedItems itemsPerPage={1} />
+      <PaginatedItems itemsPerPage={3} />
     </>
   );
 };
