493e613e887799f2c3270cfc0accc875a803d901
yattdev <yattdeveloper@gmail.com>
2022-01-05 09:48:23 +0100

    Update Category page, to implement it

--- src/pages/blog/Category.jsx
+++ src/pages/blog/Category.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+// import React from "react";
+import Header from "../../components/Header";
+import PaginatedItems from "../../components/utils/PaginatedItems";
+import HeaderImage from "../../assets/img/website_progess.png";
+import useFetch from "../../components/hooks/useFetch";
+import CategoryItemList from "../../components/blog/category/CategoryItemList";
+import { useState, useEffect } from "react";
 
 const Category = () => {
-  return <>Category</>;
+  const [categoriesList, setCategoriesList] = useState([]);
+  const { items } = useFetch("categories");
+
+  useEffect(() => {
+    setCategoriesList(() => {
+      return items;
+    });
+  }, [items]);
+
+  if (categoriesList.length === 0) {
+    return (
+      <>
+        <center>
+          <em>
+            <h3>Loading ...</h3>
+          </em>
+        </center>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {/*<!-- Header -->*/}
+      <Header
+        title="Blog en developpement..."
+        sub_title="Technologie blog, for developers and Entrepreneurs"
+        image={HeaderImage}
+      />
+      {/*<!-- CategoryItemList -->*/}
+      <PaginatedItems
+        ItemList={CategoryItemList}
+        itemsPerPage={3}
+        items={categoriesList}
+      />
+    </>
+  );
 };
 
 export default Category;
