47190a5440664157f52eadadc7562d28ded07473
yattdev <yattdeveloper@gmail.com>
2022-01-07 16:16:52 +0100

    Update component ArticleItemList, to add bootdey template, for articles list

--- src/components/blog/article/ArticleItemList.jsx
+++ src/components/blog/article/ArticleItemList.jsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import { useGlobalContext } from "../../../context";
 import ArticleItemDetails from "./ArticleItemDetails";
 
+// Style css for list of articles
+import "../../../assets/css/articlesList.css";
+
 const ArticleItemList = ({ items }) => {
   const [articles, setArticles] = useState([]);
   const { loading } = useGlobalContext();
@@ -41,13 +44,15 @@ const ArticleItemList = ({ items }) => {
   return (
     <>
       {/*<!-- Main Content-->*/}
-      <div className="container px-4 px-lg-5">
+      <div className="container itemsList pb-5 mb-3">
         <div className="row gx-4 gx-lg-5 justify-content-center">
-          <div className="col-md-10 col-lg-8 col-xl-7">
-            {/* <!-- Post preview-->*/}
-            {articles.map((item) => {
-              return <ArticleItemDetails key={item.id} {...item} />;
-            })}
+          <div className="col-12">
+            <div className="row">
+              {/* <!-- Post preview-->*/}
+              {articles.map((item) => {
+                return <ArticleItemDetails key={item.id} {...item} />;
+              })}
+            </div>
           </div>
         </div>
       </div>
