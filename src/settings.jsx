393c2c7fcd34d3605bfe2902b40310863985a33a
yattdev <yattdeveloper@gmail.com>
2022-01-07 13:08:33 +0100

    Update settings to change backend_base_url, to point it towards my remote server

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,4 +1,4 @@
-const backend_base_url = "https://backend.yatt.tech/api/v1/";
+const backend_base_url = "http://blog.remote.yatt.tech/api/v1/";
 export const ROOTPATH = "./";
 
 export default backend_base_url;
