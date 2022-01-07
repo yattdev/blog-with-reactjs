0885709d8237c1d5a96dacbb603bc952eb40724b
yattdev <yattdeveloper@gmail.com>
2022-01-07 10:36:35 +0100

    Create a new loading component:
    - That can use wherever i want, and auto-update on all component it use it

---
+++ src/components/utils/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <center>
+        <em>
+          <h3>Loading ...</h3>
+        </em>
+      </center>
+    </>
+  );
+};
+
+export default Loading;
