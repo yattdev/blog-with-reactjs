6f70a0c8eab8c5222b32ef3d3f2eb6014e5905dc
yattdev <yattdeveloper@gmail.com>
2022-01-07 11:31:41 +0100

    UPdate CategoryDetails
    - To implement Article component when fetch list of articles of category

--- src/pages/blog/CategoryDetails.jsx
+++ src/pages/blog/CategoryDetails.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../components/hooks/useFetch";
+import Loading from "../../components/utils/Loading";
+import { useGlobalContext } from "../../context";
+import Article from "./Article";
 
 const CategoryDetails = () => {
-  return <>CategoryDetails</>;
+  let { categoryId, categoryName } = useParams();
+  const { loading } = useGlobalContext();
+  const [categoryArticles, setCategoryArticles] = useState([]);
+  const { items } = useFetch(
+    `categorie-articles/${categoryId}/${categoryName}`
+  );
+
+  const update_categoryArticles = useCallback(() => {
+    if (items) {
+      setCategoryArticles(items);
+    }
+  }, [items]);
+
+  useEffect(() => {
+    update_categoryArticles();
+  }, [update_categoryArticles]);
+
+  if (categoryArticles.length === 0) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Article props={items} />
+    </>
+  );
 };
 
 export default CategoryDetails;
